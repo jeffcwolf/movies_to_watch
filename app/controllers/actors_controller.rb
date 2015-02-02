1beb8f33c68298981128ccc64bea7f29fe9231bf
Jeff Wolf <jeffcwolf@gmail.com>
2015-02-02 10:31:24 -0600

    Creates Destroy action

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,12 +1,21 @@
 class ActorsController < ApplicationController
 
   def index
+
     @actors = Actor.all
+
   end
 
   def show
 
     @actor = Actor.find(params[:id])
+
+  end
+
+  def destroy
+
+    @actor = Actor.find(params[:id])
+    @actor.destroy
 
   end
 
